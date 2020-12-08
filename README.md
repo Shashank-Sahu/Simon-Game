c3e0aefa8825f83c72f121c0bd768739f8c57e89
Shashank-Sahu <51028590+Shashank-Sahu@users.noreply.github.com>
2020-12-08 23:19:47 +0530

    Simon Game

---
+++ README.md
@@ -0,0 +1,32 @@
+# Simon Game
+
+As the name suggests, you have to press the buttons you have been told to.
+When a new color is flashed, you have to push the buttons from the very beginning of the pattern. Failing to do so will result in game over.
+
+## Disclaimer
+
+The HTML and CSS code are not mine, this project is based on the Simon Game Module on an Udemy Bootcamp Course by Angela Yu, you may find the resources for the same on the course. Only the JS file is mine, though you can also find the solution on the course as well. However I have not checked the solution code so my solution may differ from the solution in the module.
+
+## How To Play
+
+Just tap on any key on your keyboard ( please be reasonable and try to press letters, numeric or character keys as I have not tried it may not start if press function keys of shift or whatever ). Once pressed a color will flash. Click on it to view the next flash/sequence. Once a new sequence is revealed you have to click on the buttons from the beginning of the pattern. When correct pattern is followed, level number will increase. If the pattern is done wrong at any step, Game will be Over. Press again on any key on keyboard to restart. (The Game Will not Work on mobile, changes may be added for this issue).
+
+## Cheat
+
+Yes my friend you can also cheat. Like any other games there are some restrictions. However if you have played the game, you may have seen what I am talking about. By pressing a key you can start the game but you can press the keys again and get to the next level just like that. But where is the fun in that, so, when you press the keys and get to the next level you can still play the game normally and following the same pattern as it was shown when you were cheating. Now, you may ask how will I remember all the steps if I go too far ahead, Well, then just go the Chrome Developer Tools and open console and type:
+
+```bash
+pattern
+```
+
+And you will get the array.
+
+If you forgot what was the last step/click you did, then type:
+
+```bash
+playerPattern
+```
+
+And you will get the pattern that you have down since the last click/step.
+
+Feel free to download the project and edit or modify the code as per your cheeky ideas.
---
+++ game.js
@@ -0,0 +1,88 @@
+let gameOver = false;
+const colors = ["red", "green", "yellow", "blue"];
+let playerPattern = [];
+let pattern = [];
+let currentLevel;
+let started = false;
+
+$(document).keypress(function () {
+    gameOver = false;
+    nextSeq();
+});
+
+$(document).click(function () {
+    gameOver = false;
+    if (!started) {
+        nextSeq();
+        started = true;
+    }
+})
+
+$(".btn").click(function (event) {
+    ifPressed(event.currentTarget.id);
+    makeSound(event.currentTarget.id);
+    playerPattern.push(event.currentTarget.id);
+    check(playerPattern.length);
+});
+
+function makeSound(id) {
+    switch (id) {
+        case "red":
+            var red = new Audio("./sounds/red.mp3");
+            red.play();
+            break;
+        case "green":
+            var green = new Audio("./sounds/green.mp3");
+            green.play();
+            break;
+        case "blue":
+            var blue = new Audio("./sounds/blue.mp3");
+            blue.play();
+            break;
+        case "yellow":
+            var yellow = new Audio("./sounds/yellow.mp3");
+            yellow.play();
+            break;
+    }
+}
+function ifPressed(id) {
+    $("#" + id).addClass("pressed");
+    setTimeout(function () {
+        $("#" + id).removeClass("pressed");
+    }, 100)
+}
+
+function getRandom() {
+    return Math.floor(Math.random() * 4);
+}
+
+function nextSeq() {
+    var currentColor = colors[getRandom()];
+    $("#" + currentColor).fadeOut(100).fadeIn(100);
+    makeSound(currentColor);
+    pattern.push(currentColor);
+    currentLevel = pattern.length;
+    $("h1").text("Level " + currentLevel);
+}
+
+function check(level) {
+
+    if (playerPattern[level - 1] !== pattern[level - 1]) {
+        $("h1").text("GameOver Press any Key to Restart");
+        playerPattern = [];
+        pattern = [];
+        gameOver = true;
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 100);
+        var over = new Audio("./sounds/wrong.mp3");
+        over.play();
+    }
+
+    if (level === pattern.length && !gameOver) {
+        setTimeout(nextSeq, 1000);
+        playerPattern = [];
+    }
+
+}
